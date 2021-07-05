1e558068bedc24bf08206afa7e2d0ef9f66741d2
Felix Springer <felixspringer149@gmail.com>
2021-07-05 11:19:30 +0200

    Change many  to  in layers.

--- keyboards/keebio/iris/keymaps/jumper149/keymap.c
+++ keyboards/keebio/iris/keymaps/jumper149/keymap.c
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MUTE, KC_VOLU, _______, _______, _______,                            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_BRID, KC_VOLD, KC_BRIU, _______, _______,                            KC_QUOT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
+     KC_LSFT, KC_BRID, KC_VOLD, KC_BRIU, XXXXXXX, XXXXXXX,                            KC_QUOT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, _______, _______, _______, _______, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, KC_DEL,                    KC_DEL,  _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_MOD, RGB_SAI, RGB_SAD, XXXXXXX, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, RGB_VAI, RGB_VAD, XXXXXXX, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, RGB_VAI, RGB_VAD, XXXXXXX, KC_PGDN, KC_MINS, KC_LPRN,          XXXXXXX, KC_PLUS, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
